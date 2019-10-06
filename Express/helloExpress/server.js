184bc8a37221bba40562279e642217ee016a8817
Nawaf <30578987+The-7-Hunter@users.noreply.github.com>
2019-10-06 12:48:37 +0300

    upload cars and cats task for express

---
+++ Express/helloExpress/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + "/static"));
+
+
+app.get('/', (request, response) => {
+    response.send("Hello Express");
+});
+
+app.get('/new', (request, response) => {
+    response.send("this is new");
+});
+
+app.get('/users', (request, response) => {
+    var users_array = [
+        {name : "nawaf", email: "nawaf@site.sa"},
+        {name : "nawaf", email: "nawaf@site.sa"},
+        {name : "nawaf", email: "nawaf@site.sa"},
+        {name : "nawaf", email: "nawaf@site.sa"},
+        {name : "nawaf", email: "nawaf@site.sa"},
+    ];
+response.render("users", {"users": users_array})});
+app.listen(8000, () => console.log("listening on port 8000"));
