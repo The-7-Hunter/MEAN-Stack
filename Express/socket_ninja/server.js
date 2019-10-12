2d7b7cdf14b1e357153d9de95756fc997afad477
Nawaf <30578987+The-7-Hunter@users.noreply.github.com>
2019-10-12 23:28:55 +0300

    easier way of implementing socket

--- Express/socket_ninja/server.js
+++ Express/socket_ninja/server.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var socket = require('socket.io');
-
 var app = express();
+
+app.use(express.static(__dirname + "public"));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 // this is an alternative way to do it using arrow functions
 // var server = app.listen(8000, () => console.log("listening on port 8000!"));
@@ -11,16 +13,17 @@ var server = app.listen(8000, function () {
     console.log("listening on port 8000!");
 });
 
-// serve content using express
-app.use(express.static('public'));
 
-// pass server to socket, like saying that hey socket work on this server 
-var io = socket(server);
+app.get('/', function(req, res){
+    res.render('index');
+})
 
-// ON means hey server listen to this action then trigger
+
+const io = require('socket.io')(server);
 io.on('connection', function(socket){
     console.log("we are connected to a socket!", socket.id);
-    socket.on('form', function(info){
-        io.sockets.emit('form', info);
-    });
-})
+    socket.on('post_form', function(data){
+        io.sockets.emit('show', {info: data.info})
+    })
+});
+
