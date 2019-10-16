657d7665f1a0ebaf417c8e9d9b66dc8fac7cd27e
Nawaf <30578987+The-7-Hunter@users.noreply.github.com>
2019-10-16 16:12:15 +0300

    structuring code in an MVC way

--- mongoDB/hello_mongoose/server.js
+++ mongoDB/hello_mongoose/server.js
@@ -10,32 +10,7 @@ app.set('views', __dirname + '/views');
 
 mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
 
-const QuoteSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    quote: {type: String, required: true}
-}, {timestamps: { createdAt: true, updatedAt: false }});
-
-const Quote = mongoose.model('Quote', QuoteSchema);
-
-app.get('/', (req, res) => {
-    res.render("index")
-});
-
-app.get('/quotes', (req, res) => {
-    Quote.find().sort({_id: -1})
-        .then(data => res.render("quotes", { quotes: data }))
-        .catch(err => res.json(err));
-});
-
-app.post('/quotes', (req, res) => {
-    
-    const quote = new Quote(req.body);
-    quote.save()
-        .then(() => res.redirect('/quotes'))
-        .catch((err) => {
-            console.log("We have an error!", err);
-            res.redirect('/');
-        });
-});
+require('./server/models/model');
+require('./server/config/routes.js')(app)
 
 app.listen(9000, () => console.log("listening on port 9000"));
---
+++ mongoDB/hello_mongoose/server/models/model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const QuoteSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    quote: { type: String, required: true }
+}, { timestamps: { createdAt: true, updatedAt: false } });
+
+mongoose.model('Quote', QuoteSchema);
